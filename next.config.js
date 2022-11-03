b25d409352cfd85f93fb92f27edeba392b96ddbf
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-11-03 15:52:40 +0100

    Added statically.io CDN in next.config.js for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["links.papareact.com", "jsonkeeper.com", "images.unsplash.com"],
   },
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/kennie-larkson/kennie-larkson.github.io/gh-pages/"
+    : "",
 };
